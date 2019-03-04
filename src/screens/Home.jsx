884a5ac4239a4f26b99887ca2de20a94c0abef0f
Robert Damoc <robert.damoc@3pillarglobal.com>
2019-03-04 16:45:08 +0200

    Update HomePage

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -10,7 +10,20 @@ class Home extends Component {
           and what better way to practice if not by playing and having fun!
         </p>
         <p>
-          This app gathers multiple games that can be played on a grid/matrix.
+          The first version can be found here: <a href="https://github.com/robert-damoc/MinigamesInReact">MinigamesInReact</a>
+        </p>
+
+        <p>
+          I created the second one because the first one had the squares in the Board state; because of that, when we
+          updated one square we'd trigger a rerender on the entire board.
+        </p>
+
+        <p>
+          The new approach keeps the state on each square and uses `refs` to update it.
+        </p>
+
+        <p>
+          It works better on bigger grids, as much as we don't update too many squares at once.
         </p>
         <h2>Have fun and enjoy!</h2>
       </div>
